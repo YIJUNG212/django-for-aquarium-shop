a24e2c1354b3610d705783e056a4bfce24707836
YIJONG <poayung@gmail.com>
2023-04-10 00:16:35 +0800

    models.py裡的資料表有先建格式

--- myproject/aquarium/buyapp/models.py
+++ myproject/aquarium/buyapp/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
-# Create your models here.
+class Member(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    address = models.CharField(max_length=200)
+    phone = models.CharField(max_length=20)
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    EDUCATION_CHOICES = (
+        ('', 'Please select'),
+        ('H', 'High school or below'),
+        ('C', 'College'),
+        ('G', 'Graduate school'),
+    )
+    education = models.CharField(max_length=1, choices=EDUCATION_CHOICES)
+
+
+
+
+
+
+class Purchase(models.Model):
+    member = models.ForeignKey(Member, on_delete=models.CASCADE)
+    item = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    note = models.TextField()
+    shipping_status = models.CharField(max_length=20)
+    # 可以增加其他與消費相關的欄位
+
+
+class PaymentInfo(models.Model):
+    member = models.ForeignKey(Member, on_delete=models.CASCADE)
+    card_number = models.CharField(max_length=20)
+    # 這邊可以增加其他與付款相關的欄位
+
+class ShoppingCart(models.Model):
+    product_name = models.CharField(max_length=200)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    purchase_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.product_name
