f7a764a2afacf3dd62164e82f486723b176f89d2
YIJONG <poayung@gmail.com>
2023-04-25 22:41:40 +0800

    可以使用simple-jwt

--- myproject/shop/shop/urls.py
+++ myproject/shop/shop/urls.py
@@ -22,6 +22,7 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
+    #authentication_classes = [JWTAuthentication]#這行我不確定要不要加
     def get_permissions(self):
         if self.action == 'list':
             permission_classes = [permissions.AllowAny]
@@ -79,9 +80,46 @@ router.register(r'users', UserViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
+
+###額外加的JWT設定
+from rest_framework_simplejwt.views import (
+    TokenObtainSlidingView,
+    TokenRefreshSlidingView,
+)
+
+
 urlpatterns = [
     ##根目錄用了include,等於是包括這個router.urls,而這個urls剛剛才register完
     path('api/', include(router.urls)),
     #下面這行拿掉會沒有login的畫面
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    #加jwt token path
+    path('api/token/', TokenObtainSlidingView.as_view(), name='token_obtain'),
+    path('api/token/refresh/', TokenRefreshSlidingView.as_view(), name='token_refresh'),
+
 ]
+
+###JWT token範圍
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['username'] = user.name
+        # ...
+
+        return token
+###用戶手動創建 token
+from rest_framework_simplejwt.tokens import RefreshToken
+
+def get_tokens_for_user(user):
+    refresh = RefreshToken.for_user(user)
+
+    return {
+        'refresh': str(refresh),
+        'access': str(refresh.access_token),
+    }
